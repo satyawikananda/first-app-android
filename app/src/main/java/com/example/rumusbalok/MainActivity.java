9bb6f7441d74bfdbd1ffe8d5680f8b9e000f77d3
Satya Wikananda <satyawikananda456@gmail.com>
2019-11-02 21:49:35 +0800

    make calculate beam

--- app/src/main/java/com/example/rumusbalok/MainActivity.java
+++ app/src/main/java/com/example/rumusbalok/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.rumusbalok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    EditText edt_height,edt_length,edt_width;
-    Button btn_calculate;
-    TextView tvResult;
+    private EditText edt_height,edt_length,edt_width;
+    private Button btn_calculate;
+    private TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +29,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_calculate.setOnClickListener(this);
     }
 
-//    @Override
-//    public void onClick(View v) {
-//
-//    }
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_calculate){
+            String inputLength = edt_length.getText().toString().trim();
+            String inputWidth  = edt_width.getText().toString().trim();
+            String inputHeight = edt_height.getText().toString().trim();
+
+            boolean isEmptyFields = false;
+            boolean isInvalidDouble = false;
+
+            if(TextUtils.isEmpty(inputLength)){
+                isEmptyFields = true;
+                edt_length.setError("Inputan tidak boleh kosong");
+            }
+
+            if(TextUtils.isEmpty(inputHeight)){
+                isEmptyFields = true;
+                edt_height.setError("Inputan tidak boleh kosong");
+            }
+
+            if(TextUtils.isEmpty(inputWidth)){
+                isEmptyFields = true;
+                edt_width.setError("Inputan tidak boleh kosong");
+            }
+
+            Double length = toDouble(inputLength);
+            Double width  = toDouble(inputWidth);
+            Double height = toDouble(inputHeight);
+
+            if(length == null){
+                isInvalidDouble = true;
+                edt_length.setError("Inputan ini harus berupa nomor yg valid");
+            }
+
+            if(width == null){
+                isInvalidDouble = true;
+                edt_width.setError("Inputan ini harus berupa nomor yg valid");
+            }
+
+            if(height == null){
+                isInvalidDouble = true;
+                edt_height.setError("Inputan ini harus berupa nomor yg valid");
+            }
+
+            if(!isEmptyFields && !isInvalidDouble){
+                double volume = height * width * length;
+
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+    }
+
+    private Double toDouble(String str) {
+        try{
+            return Double.valueOf(str);
+        }catch (NumberFormatException e){
+            return null;
+        }
+    }
 }
