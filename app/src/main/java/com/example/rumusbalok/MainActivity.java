52937dda42e3d2fdce9f3c61169e1e20554a8737
Satya Wikananda <satyawikananda456@gmail.com>
2019-11-02 21:24:48 +0800

    ui form calculate

--- app/src/main/java/com/example/rumusbalok/MainActivity.java
+++ app/src/main/java/com/example/rumusbalok/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.rumusbalok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText edt_height,edt_length,edt_width;
+    Button btn_calculate;
+    TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edt_height = findViewById(R.id.edt_height);
+        edt_length = findViewById(R.id.edt_length);
+        edt_width = findViewById(R.id.edt_width);
+        btn_calculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+
+        btn_calculate.setOnClickListener(this);
     }
+
+//    @Override
+//    public void onClick(View v) {
+//
+//    }
 }
